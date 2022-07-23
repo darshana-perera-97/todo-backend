8e9c6a8130c026b285483c220c3deafb16a33bbb
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-23 07:56:56 +0530

    mongodb databse connected to project

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+const { default: mongoose } = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://darshana:darshana@cluster0.b4ir7g9.mongodb.net/merntutorial?retryWrites=true&w=majority"
+);
 
 app.listen(3001, () => {
   console.log("Server runs in 3001");
